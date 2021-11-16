4b6e9cffa8d0436f01a849a395d3c5fc122f4791
callmeisma <ismael.fdez87@gmail.com>
2021-11-15 19:09:04 -0500

    tidy front end, add change color,tool,size features

--- script.js
+++ script.js
@@ -1,50 +1,75 @@
 function buildGrid(unit) {
-    if (document.getElementById('gridContainer')) {
-        container = document.getElementById('gridContainer');
-        document.body.removeChild(container);
+    var gridContainer = document.getElementById('gridContainer');
+
+    if (document.getElementById('grid')) {
+        container = document.getElementById('grid');
+        console.log(container)
+        gridContainer.removeChild(container);
     }
 
     container = document.createElement('div');
-    container.setAttribute('id', 'gridContainer');
-    document.body.appendChild(container);
+    container.setAttribute('id', 'grid');
+    gridContainer.appendChild(container);
 
 
     let gridSize = unit * unit;
 
-    if (gridContainer = document.getElementById('gridContainer').hasChildNodes() === true ) {
-        let items = document.getElementById('gridContainer');
+    if (grid = document.getElementById('grid').hasChildNodes() === true ) {
+        let items = document.getElementById('grid');
         while (items.firstChild) {
             items.removeChild(items.lastChild);
         }
     }
 
     while (gridSize > 0) {
-        gridContainer = document.getElementById('gridContainer');
+        grid = document.getElementById('grid');
         gridItem = document.createElement('div');
         gridItem.setAttribute('class', 'gridItem');
-        gridContainer.appendChild(gridItem);
+        grid.appendChild(gridItem);
         gridSize --;
     }
 
-    containerCont = document.getElementById("gridContainer");
+    containerCont = document.getElementById("grid");
     containerCont.style.gridTemplateColumns = `repeat(${unit}, auto)`;
     containerCont.style.gridTemplateRows = `repeat(${unit}, auto)`;
+
+    cellFill()  
 }
 
-document.addEventListener('click', function(e){
-    if (e.target.id === 'creator') {
-        let input = document.getElementById('input').value;
-        buildGrid(input);
-    }
-    
-    cell = document.querySelectorAll('.gridItem');
+function cellFill() {
+    var cell = document.querySelectorAll('.gridItem');
+    var color = document.getElementById('color');
 
     cell.forEach((item) => {
         item.addEventListener('mouseover', function(e) {
-            e.target.style.backgroundColor = 'black';
+            if (document.getElementById('pen').classList.contains('active')) {
+                e.target.style.backgroundColor = color.value;
+            } else if (document.getElementById('eraser').classList.contains('active')) {
+                e.target.style.backgroundColor = "white";
+            }
         });
     });
+}
+
+function clearGrid() {
+    buildGrid(document.getElementById('slider').value);
+}
+
+function updateSize(unit) {
+    document.getElementById('size').innerHTML = unit + " x " + unit
+    buildGrid(unit)    
+}
+
+// Starting Grid
+buildGrid(16)
+
+// Toggle tool selection between actives
+document.addEventListener('click', function(e){
+    if (e.target.id === 'pen' ) {
+        document.getElementById('pen').classList.add("active")
+        document.getElementById('eraser').classList.remove("active")
+    } else if (e.target.id === 'eraser') {
+        document.getElementById('eraser').classList.add("active")
+        document.getElementById('pen').classList.remove("active")
+    }
 })
-
-
-
